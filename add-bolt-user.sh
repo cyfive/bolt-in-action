b4606ed826c8f09ba47ac524ffb54524c8202c55
Емец Станислав <emetssv@mail.ru>
2019-06-13 18:03:21 +0300

    Fixed permission on .ssh directory for bolt user

--- add-bolt-user.sh
+++ add-bolt-user.sh
@@ -5,7 +5,10 @@ sudo useradd -G wheel -m bolt
 echo boltpasswd | sudo passwd --stdin bolt
 
 #copy bolt public key
-sudo mkdir -p /home/bolt/.ssh
-cat /vagrant/keys/id_rsa.pub > /home/bolt/.ssh/authorized_keys
+sudo -u bolt mkdir -p /home/bolt/.ssh
+sudo -u bolt cat /vagrant/keys/id_rsa.pub > /home/bolt/.ssh/authorized_keys
 sudo chown -R bolt:bolt /home/bolt/.ssh
-sudo chmod -R 600 /home/bolt/.ssh
+sudo chmod 600 /home/bolt/.ssh/*
+sudo chmod 700 /home/bolt/.ssh
+sudo chcon -v unconfined_u:object_r:ssh_home_t:s0 /home/bolt/.ssh
+sudo chcon -R -v unconfined_u:object_r:ssh_home_t:s0 /home/bolt/.ssh
